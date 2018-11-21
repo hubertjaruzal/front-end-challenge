17b1a55085013c5242a61bc43acec8b844cff154
hubertjaruzal <hubertjaruzal@gmail.com>
2018-11-21 13:20:05 +0100

    add react-router

---
+++ src/components/Albums/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './styles.scss';
+
+
+class Albums extends Component {
+  render() {
+    return (
+      <div className='albums'>
+        albums
+      </div>
+    );
+  }
+}
+
+export default Albums;
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+import Albums from '../Albums';
 
 import './styles.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        Setapp
-      </div>
+      <Router>
+        <div className='app'>
+          <section className='content'>
+            <Switch>
+              <Route path='/' exact component={Albums} />
+              <Redirect to='/' />
+            </Switch>
+          </section>
+        </div>
+      </Router>
     );
   }
 }
