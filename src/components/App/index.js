ee91facd2e636dfd16fbb43119aa8e1994565765
hubertjaruzal <hubertjaruzal@gmail.com>
2018-11-21 12:51:36 +0100

    create basic application structure

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './styles.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Setapp
+      </div>
+    );
+  }
+}
+
+export default App;
